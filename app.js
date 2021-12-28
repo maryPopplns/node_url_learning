37d3cb90ea0c430882b407ee8ed2fb65997c27a0
Spencer Knight <spencer.knightusn@gmail.com>
2021-12-28 08:40:36 -0700

    basic server

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var http = require('http');
-var url = require('url');
+http = require('http');
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + ' ' + q.month;
-    res.end(txt);
-  })
-  .listen(8080);
+const server = http.createServer((req, res) => {
+  console.log('request was made');
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening on port 3000');
+});
